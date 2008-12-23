5a5ea332533d09bd33ba2beb71251aba3284a391
dbrechalov <dbrechalov@9aa7767e-b6f6-11dd-893d-d98698f66bbf>
2008-12-23 15:07:36 +0000

    1. Refactoring: core editor functionality has been moved to a separate class. 2. App issue with appuifw2 fixed. 3. Creating SIS-files from 0.6 available.

---
+++ trunk/makesis.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+VERSION='0.6.0'
+../Symbian/ensymble.py py2sis --uid=0xe3e34da2 --appname="S60 HTML Editor" --shortcaption="HTMLEd" --version=$VERSION --vendor="D.Mych" --verbose s60htmled.py
