03c610ee366728b79fcf8edb6ad0a4fb22f9d8f1
dbrechalov <dbrechalov@9aa7767e-b6f6-11dd-893d-d98698f66bbf>
2008-12-27 17:40:12 +0000

    class xText moved to the separate module.

--- trunk/makesis.sh
+++ trunk/makesis.sh
@@ -3,8 +3,9 @@ VERSION='0.7.1'
 # make distro
 rm -rf build
 mkdir -p build
-cp utils.py build/
-cp s60htmled.py build/default.py
+cp -v utils.py build/
+cp -v xtext.py build/
+cp -v s60htmled.py build/default.py
 # create sis-file
 ../Ensymble/ensymble.py py2sis --uid=0xe3e34da2 --appname="S60 HTML Editor" --shortcaption="HTMLEd" --version=$VERSION --vendor="D.Mych" --verbose build s60htmled-${VERSION}.sis
 rm -rf build
