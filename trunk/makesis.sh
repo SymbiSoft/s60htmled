4d253bcb5e8f82dae115a2a6f98e0edfe7cd9a52
dbrechalov <dbrechalov@9aa7767e-b6f6-11dd-893d-d98698f66bbf>
2008-12-27 17:31:19 +0000

    Make process improved: make 'build' dir with all necessary and build sis from the dir.

--- trunk/makesis.sh
+++ trunk/makesis.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-VERSION='0.7.0'
-../Symbian/ensymble.py py2sis --uid=0xe3e34da2 --appname="S60 HTML Editor" --shortcaption="HTMLEd" --version=$VERSION --vendor="D.Mych" --verbose s60htmled.py
+VERSION='0.7.1'
+# make distro
+rm -rf build
+mkdir -p build
+cp utils.py build/
+cp s60htmled.py build/default.py
+# create sis-file
+../Ensymble/ensymble.py py2sis --uid=0xe3e34da2 --appname="S60 HTML Editor" --shortcaption="HTMLEd" --version=$VERSION --vendor="D.Mych" --verbose build s60htmled-${VERSION}.sis
+rm -rf build
